d46cb49e6fe53a87213f3619f08bcb526375f2c1
guicustodio <guiilherme_custodiio@hotmail.com>
2022-05-08 21:10:29 -0300

    First Commit

--- HyperCasual 3D/Assets/Scripts/PlayerMovement.cs
+++ HyperCasual 3D/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,26 +9,38 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private int dashForce;
     [SerializeField] private float dashDuration;
-    
+    [SerializeField] private int jumpForce = 8;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        string currentSceneName = SceneManager.GetActiveScene().name;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = Vector3.up * 5;
+            rb.velocity = Vector3.up * jumpForce;
 
         }
 
         if(Input.GetKeyDown(KeyCode.RightArrow))
         {
             StartCoroutine(DashTime());
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.position.x < Screen.width / 2) 
+            {
+                rb.velocity = Vector3.up * jumpForce;
+            }
+            else if (touch.position.x > Screen.width / 2) 
+            {
+                StartCoroutine(DashTime());
+            }
         }
 
     }
@@ -49,6 +62,21 @@ public class PlayerMovement : MonoBehaviour
             }
     }
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Obstacle")) 
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        if (col.gameObject.CompareTag("Goal"))
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene("SampleScene");
+        }
+
+    }
 
 
 
